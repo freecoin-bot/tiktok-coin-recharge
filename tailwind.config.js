c20d42aa6aef5fe5ef798337030224a5ac33547c
Bikash Khanal <63199947+Lbikashofficial130@users.noreply.github.com>
2023-06-03 11:46:14 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+   
+  },
+  plugins: [],
+}
